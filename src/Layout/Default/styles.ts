8755f2691c1082998e86a3fa836d38b1d36becc9
Igor Santos <falaigors@gmail.com>
2022-11-09 00:37:01 -0300

    style layout

--- src/Layout/Default/styles.ts
+++ src/Layout/Default/styles.ts
@@ -3,9 +3,10 @@ import { styled } from "../../styles";
 export const LayoutContainer = styled("main", {
   display: "flex",
   flexDirection: "column",
-  margin: "0 auto",
 });
 
 export const LayoutWrapper = styled("section", {
   maxWidth: "1280px",
+  width: "100%",
+  margin: "0 auto",
 });
