9b725ea427a915242afdedb77a27140e8b6c0fe4
Igor Santos <falaigors@gmail.com>
2022-11-10 11:21:52 -0300

    create classroom route

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,14 +1,20 @@
 import { Routes, Route } from "react-router-dom";
-import { DefaultLayout } from "@/Layout/Default";
 import { HomePage } from "@/pages/Home";
 import { CoursesPage } from "@/pages/Courses";
+
+import { DefaultLayout } from "@/Layout/Default";
+import { ClassroomLayout } from "@/Layout/Classroom";
 
 export function AppRouter() {
   return (
     <Routes>
       <Route path="/" element={<DefaultLayout />}>
         <Route path="/" element={<HomePage />} />
-        <Route path="/courses" element={<CoursesPage />} />
+        <Route path="courses" element={<CoursesPage />} />
+      </Route>
+
+      <Route path="/slug" element={<ClassroomLayout />}>
+        <Route path="" element={<HomePage />} />
       </Route>
     </Routes>
   );
